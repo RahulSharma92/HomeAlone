249a49e84a325e51fdeb0384eb43c43b7ef01fc7
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2021-05-09 18:03:24 +0530

    update code to run on heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const hostName = '127.0.0.1';
-const port = '3000';
+
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -8,6 +8,6 @@ const server = http.createServer((req, res) => {
     res.end('Hello World');
 });
 
-server.listen(port, hostName, () => {
-    console.log(`Server running at http://${hostName}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at ${port}`);
 });
